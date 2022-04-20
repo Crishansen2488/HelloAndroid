d671526a2c2f3d255ed4296808261430b86a4538
CSC <cris-hansen@hotmail.com>
2022-04-20 14:54:37 -0300

    Alteração teste 1

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.helloandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//comentário de alteração 1.1
+//comentário de  alteração 1.1
 class MainActivity : AppCompatActivity() {
     //teste 1
     override fun onCreate(savedInstanceState: Bundle?) {
